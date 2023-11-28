d2eb5e77362548ea44aa57c7bd5ee48afcb0d3e5
Mateus Roveda <mateus.roveda@universo.univates.br>
2023-11-28 20:12:26 -0300

    Implementa CRUD de usuários com DAO e Controller

---
+++ 29-11-php-db-oo-controller/App/Models/Usuario.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+class Usuario
+{
+    private $id;
+    private $nome;
+
+    public function __construct($nome, $id = null)
+    {
+        $this->nome = $nome;
+        $this->id = $id;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function __toString()
+    {   
+        return json_encode([
+            'id' => $this->id,
+            'nome' => $this->nome
+        ]);
+    }
+}
