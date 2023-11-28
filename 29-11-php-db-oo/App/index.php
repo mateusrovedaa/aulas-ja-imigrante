5d6650ae717f218c6aa0a2c094d51b330e4c1e1e
Mateus Roveda <mateus.roveda@universo.univates.br>
2023-11-28 19:36:52 -0300

    Implementa OO com DB

---
+++ 29-11-php-db-oo/App/index.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\Database;
+use App\Model\User;
+
+$db = new Database();
+
+do {
+    echo "
+    [1] Cadastrar usuário
+    [2] Listar usuários (utilizando for)
+    [3] Listar 1 usuário
+    [4] Apagar 1 usuário
+    [5] Sair";
+    echo "\n";
+    $opcao = readline("Digite a opção desejada: ");
+
+    if ($opcao == 1) {
+        $nome = readline("Digite um nome: ");
+        $usuario = new User($nome);
+        $connection = $db->getConnection();
+        $sql = "INSERT INTO teste (nome) VALUES ('" . $usuario->getName() . "')";
+        $stmt = $connection->query($sql);
+    } else if ($opcao == 2) {
+        $connection = $db->getConnection();
+        $sql = "SELECT * FROM teste ORDER BY id";
+        $stmt = $connection->query($sql);
+        $usuariosDB = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+
+        foreach ($usuariosDB as $usuario) {
+            echo "ID: " . $usuario['id'] . "\n";
+            echo "Nome: " . $usuario['nome'] . "\n";
+        }
+    } else if ($opcao == 3) {
+        $id = readline("Digite o id do usuário: ");
+        $connection = $db->getConnection();
+        $sql = "SELECT * FROM teste WHERE id = $id";
+        $stmt = $connection->query($sql);
+        $usuariosDB = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        $usuario = new User($usuariosDB[0]['nome'], $usuariosDB[0]['id']);
+        echo $usuario;
+
+    } else if ($opcao == 4) {
+        $id = readline("Digite o id do usuário para apagar: ");
+        $connection = $db->getConnection();
+        $sql = "DELETE FROM teste WHERE id = $id";
+        $connection->query($sql);
+    }
+} while ($opcao != 5);
+$db->closeConnection();
